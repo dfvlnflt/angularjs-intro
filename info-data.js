43876b076b96e77ff037817c41915403e7d726a1
dfvlnflt <dfvlnflt06@gmail.com>
2016-04-02 13:40:58 -0400

    Update info-data.js

--- info-data.js
+++ info-data.js
@@ -1,1 +1,13 @@
-
+angular.module('info').value('infoListValue', [
+  {
+    firstname: 'Danielle',
+    lastname: 'Frantz',
+    age: '27',
+    gender: 'Female'
+  },
+  {
+    firstname: 'Jeronimo',
+    lastname: 'Matos',
+    age: '27',
+    gender: 'Male'
+  }])
